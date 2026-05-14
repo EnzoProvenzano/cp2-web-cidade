6970deda459b5a1031005ba61e33cb68c56986a1
enzo <enzoprovenzano123@gmail.com>
2026-05-14 15:01:10 -0300

    fix: modificando e melhorando o index.html, loja.html e o script.js

--- src/js/script.js
+++ src/js/script.js
@@ -1,53 +1,57 @@
 const produtos = [
   {
-    nome: "Homem-Aranha",
-    preco: 299.90,
-    imagem: "./src/assets/img/homem-aranha.jpg"
-  },
-
-  {
     nome: "Batman",
     preco: 349.90,
-    imagem: "./src/assets/img/batman.jpg"
-  },
-
-  {
-    nome: "Capitão América",
-    preco: 279.90,
-    imagem: "./src/assets/img/capitao.jpg"
+    imagem: "../src/assets/img/batman.png"
   },
 
   {
     nome: "Goku",
     preco: 399.90,
-    imagem: "./src/assets/img/goku.jpg"
+    imagem: "../src/assets/img/goku.png"
+  },
+
+  {
+    nome: "Homem-Aranha",
+    preco: 299.90,
+    imagem: "../src/assets/img/homem-aranha.png"
   },
 
   {
     nome: "Homem de Ferro",
     preco: 459.90,
-    imagem: "./src/assets/img/h-ferro.png"
+    imagem: "../src/assets/img/homem-ferro.png"
+  },
+
+  {
+    nome: "Capitão América",
+    preco: 279.90,
+    imagem: "../src/assets/img/capitao-america.png"
   }
 ];
 
 const container = document.getElementById("cards-container");
 
-produtos.forEach(produto => {
+if (container) {
 
-  container.innerHTML += `
-  
-    <div class="card">
-      <img src="${produto.imagem}" alt="${produto.nome}">
-      
-      <h2>${produto.nome}</h2>
+  produtos.forEach(produto => {
 
-      <p>R$ ${produto.preco.toFixed(2)}</p>
+    container.innerHTML += `
+    
+      <div class="card">
+        <img src="${produto.imagem}" alt="${produto.nome}">
+        
+        <h2>${produto.nome}</h2>
 
-      <button>Comprar</button>
-    </div>
-  
-  `;
-});
+        <p>R$ ${produto.preco.toFixed(2)}</p>
+
+        <button>Comprar</button>
+      </div>
+    
+    `;
+  });
+
+}
 
 const carrinho = [
   {
